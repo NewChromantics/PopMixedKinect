5b51be3be9421ab4fa1011b84b864b593a6ced1f
Graham Reeves <graham@grahamreeves.com>
2017-01-25 11:43:49 +0000

    added popcast recording & PopMovieKinect simple script for handling mulitple streams

---
+++ Assets/PopMovieKinect.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+using System.Collections;
+
+
+/// <summary>
+///		PopMovieSimple is a very simple example of how to use a PopMovie instance to
+///		play a movie to a specified texture using games timedelta to control the playback.
+/// </summary>
+[AddComponentMenu("PopMovie/PopMovieKinect")]
+public class PopMovieKinect : MonoBehaviour {
+
+	public Texture			ColourTexture;
+	public Texture			DepthTexture;
+	public string			Filename = "kinect:0";
+
+	public PopMovieParams	Parameters;
+	public PopMovie			Movie;
+	float					MovieTime = 0;
+
+	public bool				PlayOnAwake = true;
+
+	[Tooltip("Enable debug logging when movie starts. Same as the global option, but automatically turns it on")]
+	public bool				EnableDebugLog = false;
+
+	void Awake() {
+
+		if (PlayOnAwake ) {
+			Play ();
+		}
+	}
+
+	public void Play()
+	{
+		if (Movie == null) {
+			Movie = new PopMovie (Filename, Parameters, MovieTime);
+			if ( EnableDebugLog )
+				PopMovie.EnableDebugLog = true;
+		}
+
+	}
+
+	public void Stop()
+	{
+		if ( Movie != null )
+		{
+			Movie.Free ();
+			Movie = null;
+		}
+		MovieTime = 0;
+	}
+
+	public void UpdateTextures()
+	{
+		if (Movie != null && ColourTexture != null)
+			Movie.UpdateTexture (ColourTexture, 0);
+
+		if (Movie != null && DepthTexture != null)
+			Movie.UpdateTexture (DepthTexture, 1);
+	}
+
+	public void Update () {
+
+		if (Movie != null)
+		{
+			MovieTime += Time.deltaTime;
+			Movie.SetTime (MovieTime);
+
+			UpdateTextures();
+		}
+
+	}
+
+	
+}
---
+++ Assets/RecordVideoTexture.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class RecordVideoTexture : MonoBehaviour {
+
+	[InspectorButton("Stop")]
+	public bool				_Stop;
+
+	public Texture			Input;
+	public PopCastParams	Params;
+	public string			Filename = "file:streamingassets/*.mp4";
+	public PopCast			Cast;
+	public bool				EnableDebugLog = true;
+
+	void Start()
+	{
+		if ( Params == null )
+			Params = new PopCastParams();
+
+		if ( Application.isPlaying )
+		{
+			Cast = new PopCast( Filename, Params );
+			PopCast.EnableDebugLog = EnableDebugLog;
+		}
+	}
+
+	void Update () {
+	
+		if ( Cast != null && Input != null )
+			Cast.UpdateTexture( Input, 0 );
+		
+		PopCast.Update();
+	}
+
+	public void Stop()
+	{
+		if ( Cast != null )
+		{
+			Debug.Log("Freeing cast");
+			Cast.Free();
+			Cast = null;
+		}
+	}
+
+}
